a64789ab41b4b9a059563c0972a40887263b1e53
Hans <fb_hans@outlook.com>
2021-01-11 17:15:20 +0800

    Game 1 Skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Challenging Games
+There are seven challenging games in this project for you to play.
+First: Calculate your age in days
---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+
+// Challenge 1: Calculate Your Age in Days
+function ageInDays(){
+    let ageInYear = prompt("Hey Buddy, which year did your born?")
+    let ageInDays = (2021-ageInYear) * 365
+    console.log(ageInDays)
+}
