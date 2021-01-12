dd5afea4bbff2ad721c80b9c6899f357e327c712
Hans <fb_hans@outlook.com>
2021-01-12 09:46:06 +0800

    Game 2

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,21 @@ function ageInDays(){
     let ageInYear = prompt("Hey Buddy, which year did your born?")
     let ageInDays = (2021-ageInYear) * 365
     console.log(ageInDays)
+    let h1 = document.createElement('h1')
+    let result = document.createTextNode("You are " + ageInDays + " years old.")
+    h1.appendChild(result)
+    document.querySelector("#ageid-result").appendChild(h1);
+}
+
+function resetResultDiv(){
+    document.querySelector("#ageid-result h1").remove();
+}
+
+// Challenge 2 : Generate Cats
+document.querySelector("#gen-cat-btn").addEventListener("click", catGenerator)
+
+function catGenerator(){
+    let img = document.createElement("img")
+    img.src = "http://thecatapi.com/api/images/get?mime_types=gif&size=small"
+    document.querySelector(".flex-container-cat-gen").appendChild(img)
 }
