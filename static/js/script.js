8580029c27611ec5c7cf79931f98665f9e2d4d3e
Hans <fb_hans@outlook.com>
2021-01-12 11:16:24 +0800

    Game 3

--- static/js/script.js
+++ static/js/script.js
@@ -22,3 +22,71 @@ function catGenerator(){
     img.src = "http://thecatapi.com/api/images/get?mime_types=gif&size=small"
     document.querySelector(".flex-container-cat-gen").appendChild(img)
 }
+
+// Challenge 3 : Rock Paper Scissors Game
+function rpsGame(chosenItem){
+    let yourChoice = chosenItem.id
+    let botChoice = randomChoices(randomNumber(3))
+    console.log(yourChoice, botChoice)
+    result = computeWinner(yourChoice, botChoice)
+    message = convertToMessage(result)
+    console.log(message)
+    displayFinalMessage(message, yourChoice, botChoice)
+}
+
+function randomNumber(upperLimit){
+    return Math.floor(Math.random() * upperLimit) + 1
+}
+
+function randomChoices(number){
+    let choiceReflet = {
+        1 : 'rock',
+        2 : 'paper',
+        3 : 'scissors',
+    }
+    return choiceReflet[number]
+}
+
+function computeWinner(yourChoice, botChoice){
+    let scoreMap = {
+        'rock' : {'rock': 0.5, 'paper': 0, 'scissors': 1},
+        'paper' : {'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors' : {'rock': 0, 'paper': 1, 'scissors': 0.5},
+    }
+
+    return scoreMap[yourChoice][botChoice]
+}
+
+function convertToMessage(result){
+    let messageMap = {
+        0 : {'message': 'You lost!', 'color': 'red'},
+        0.5 : {'message': 'You drew!', 'color': 'yellow'},
+        1 : {'message': 'You won!', 'color': 'green'},
+    }
+    return messageMap[result]
+}
+
+function displayFinalMessage(message, yourChoice, botChoice){
+    let imageMap = {
+        'rock': document.querySelector("#rock").src,
+        'paper': document.querySelector("#paper").src,
+        'scissors': document.querySelector("#scissors").src,
+    }
+
+    document.querySelector("#rock").remove()
+    document.querySelector("#paper").remove()
+    document.querySelector("#scissors").remove()
+
+    let yourImg = document.createElement("div")
+    let botImg = document.createElement("div")
+    let finalMessage = document.createElement("div")
+
+    yourImg.innerHTML = `<img src=${imageMap[yourChoice]} width=150 height=150 style="box-shadow: 5px 5px 50px blue">`
+    finalMessage.innerHTML =   `<h1 style="color: ${message['color']};padding: 30px;font-size:60px;">${message['message']}</h1>`
+    botImg.innerHTML = `<img src=${imageMap[botChoice]} width=150 height=150 style="box-shadow: 5px 5px 50px red">`
+
+    document.querySelector(".flex-container-rps").appendChild(yourImg)
+    document.querySelector(".flex-container-rps").appendChild(finalMessage)
+    document.querySelector(".flex-container-rps").appendChild(botImg)
+
+}
